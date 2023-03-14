c5f42af728e4061850f0cfd78b8cb7cbd2321d49
Dromich <hrushch.rr57@gmail.com>
2023-03-14 10:49:51 +0200

    V 1.1

--- make_hls_cut&decode.bat
+++ make_hls_cut&decode.bat
@@ -1,5 +1,5 @@
 for  %%i in (*.mp4) do (  
-   echo Нарізка i декодування в HLS файла %%i 
+   echo Нарізка i декодування в HLS файл -  %%i 
    echo  %%~ni
    md %%~ni
   ffmpeg -i %%i -profile:v baseline -level 3.0  -start_number 0 -hls_time 2 -hls_list_size 0  -f hls ./%%~ni/index.m3u8
--- make_hls_cut_only.bat
+++ make_hls_cut_only.bat
@@ -1,7 +1,8 @@
 for  %%i in (*.mp4) do (  
-   echo Нарізка в HLS файлу %%i 
+   echo Нарізка в HLS файлу -  %%i 
    echo  %%~ni
    md %%~ni
   ffmpeg -i %%i -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls ./%%~ni/index.m3u8
   
 )
+
